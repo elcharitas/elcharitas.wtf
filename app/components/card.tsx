a5149174b8806575b79e75d660c640606557d184
elcharitas <jonathanirhodia@gmail.com>
2023-08-29 04:33:44 +0100

    fix: improve card animations

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,43 +1,16 @@
 "use client";
-import {
-	motion,
-	useMotionTemplate,
-	useMotionValue,
-	useSpring,
-} from "framer-motion";
-
-import { MouseEventHandler, PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 
 export const Card: React.FC<PropsWithChildren> = ({ children }) => {
-	const mouseX = useSpring(0, { stiffness: 500, damping: 100 });
-	const mouseY = useSpring(0, { stiffness: 500, damping: 100 });
+  return (
+    <div className="overflow-hidden relative transition-all duration-700 ease-in-out transform hover:scale-105 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600 shadow-lg hover:shadow-2xl">
+      <div className="pointer-events-none">
+        <div className="absolute inset-0 z-0 transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
+        <div className="absolute inset-0 z-10 bg-gradient-to-br opacity-100 via-zinc-700/10 transition duration-1000 group-hover:opacity-50" />
+        <div className="absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100" />
+      </div>
 
-	function onMouseMove({ currentTarget, clientX, clientY }: any) {
-		const { left, top } = currentTarget.getBoundingClientRect();
-		mouseX.set(clientX - left);
-		mouseY.set(clientY - top);
-	}
-	let maskImage = useMotionTemplate`radial-gradient(240px at ${mouseX}px ${mouseY}px, white, transparent)`;
-	let style = { maskImage, WebkitMaskImage: maskImage };
-
-	return (
-		<div
-			onMouseMove={onMouseMove}
-			className="overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600 "
-		>
-			<div className="pointer-events-none">
-				<div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
-				<motion.div
-					className="absolute inset-0 z-10  bg-gradient-to-br opacity-100  via-zinc-100/10  transition duration-1000 group-hover:opacity-50 "
-					style={style}
-				/>
-				<motion.div
-					className="absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100"
-					style={style}
-				/>
-			</div>
-
-			{children}
-		</div>
-	);
+      {children}
+    </div>
+  );
 };

a200ec61999058b13456278f102110a658feee3a
elcharitas <jonathanirhodia@gmail.com>
2023-08-29 06:04:47 +0100

    feat: fetch all repos across orgs

--- app/(posts)/projects/page.tsx
+++ app/(posts)/projects/page.tsx
@@ -2,21 +2,31 @@ import { PostsPage } from "../post-page";
 
 export default async function Page() {
   const response = await fetch(
-    "https://api.github.com/users/elcharitas/repos?sort=stars"
+    "https://api.github.com/user/repos?sort=stars&affiliation=owner,collaborator&per_page=100&direction=desc",
+    {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    }
   );
   const data = await response.json();
 
-  const filteredData = data.filter((repo: Repo) => !repo.fork);
+  const filteredData: Repo[] =
+    data.filter?.((repo: Repo) => !repo.fork && repo.stargazers_count) ?? [];
 
-  const [featured, ...projects] = filteredData.map((repo: Repo) => ({
-    slug: repo.name,
-    title: repo.name,
-    brief: repo.description,
-    date: repo.created_at,
-    url: repo.html_url,
-    content: "",
-    coverImage: repo.owner.avatar_url,
-  }));
+  const [featured, ...projects]: Post[] = filteredData
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .map((repo) => ({
+      slug: repo.name,
+      title: `${repo.owner.login}/${repo.name}`,
+      brief: repo.description,
+      date: repo.created_at,
+      url: repo.html_url,
+      content: "",
+      coverImage: repo.owner.avatar_url,
+      type: "projects",
+      views: repo.stargazers_count,
+    }));
 
   return <PostsPage title="Projects" featured={featured} sorted={projects} />;
 }

dbd687531363cd3f65dd96360310dfcb2d46af61
elcharitas <jonathanirhodia@gmail.com>
2023-09-03 15:03:54 +0100

    feat: improve adventure with animations and dropdown logic

---
+++ app/(posts)/adventures/adventure.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState, FC } from "react";
+import { Card } from "@/app/components/card";
+
+interface AdventureProps {
+  year: string;
+  yearData: {
+    month: string;
+    info: string[];
+  }[];
+}
+const Adventure: FC<AdventureProps> = ({ year, yearData }) => {
+  const [isExpanded, setIsExpanded] = useState(true);
+  return (
+    <li className="mb-2 flex flex-col items-center">
+      <div className="flex flex-col md:w-3/4">
+        <span
+          tabIndex={0}
+          role="button"
+          className="text-zinc-500 font-display animate-title text-4xl text-center my-8 py-8 border-zinc-800 border rounded-xl"
+          onClick={() => setIsExpanded((prev) => !prev)}
+        >
+          {year}
+        </span>
+        {isExpanded &&
+          yearData.map(({ month, info }, index) => (
+            <div
+              className={`mb-4 md:ml-4 md:mt-8 animate-fade-in md:flex flex-col ${
+                index % 2 ? "items-start" : "items-end"
+              }`}
+            >
+              <Card>
+                <div className="m-4 min-w-[300px]">
+                  <h3 className="text-lg font-display uppercase">{month}</h3>
+                  {info.map((data) => (
+                    <p key={data} className="text-zinc-400">
+                      &#8226; &nbsp; {data}
+                    </p>
+                  ))}
+                </div>
+              </Card>
+            </div>
+          ))}
+      </div>
+    </li>
+  );
+};
+
+export { Adventure };
--- app/(posts)/adventures/page.tsx
+++ app/(posts)/adventures/page.tsx
@@ -1,13 +1,16 @@
 import { Card } from "@/app/components/card";
 import adventures from "./adventures.json";
+import { Adventure } from "./adventure";
 
 const adventuresByYear = Object.entries(adventures)
   .map(([year, yearData]) => ({
     year,
-    yearData: Object.entries(yearData).map(([month, info]) => ({
-      month,
-      info,
-    })),
+    yearData: Object.entries(yearData)
+      .map(([month, info]) => ({
+        month,
+        info,
+      }))
+      .reverse(),
   }))
   .reverse();
 
@@ -27,33 +30,7 @@ export default async function Page() {
       <div className="max-w-full w-full mx-auto lg:mx-0">
         <ul className="mt-4 text-zinc-400">
           {adventuresByYear.map(({ year, yearData }) => (
-            <li key={year} className="mb-2 flex flex-col items-center">
-              <div className="flex flex-col md:w-3/4">
-                <span className="text-zinc-500 font-display text-4xl text-center my-8 py-8 border-zinc-800 border rounded-xl">
-                  {year}
-                </span>
-                {yearData.map(({ month, info }, index) => (
-                  <div
-                    className={`mb-4 md:ml-4 md:mt-8 md:flex flex-col ${
-                      index % 2 ? "items-start" : "items-end"
-                    }`}
-                  >
-                    <Card>
-                      <div className="m-4 min-w-[300px]">
-                        <h3 className="text-lg font-display uppercase">
-                          {month}
-                        </h3>
-                        {info.map((data) => (
-                          <p key={data} className="text-zinc-400">
-                            &#8226; &nbsp; {data}
-                          </p>
-                        ))}
-                      </div>
-                    </Card>
-                  </div>
-                ))}
-              </div>
-            </li>
+            <Adventure key={year} year={year} yearData={yearData} />
           ))}
         </ul>
       </div>

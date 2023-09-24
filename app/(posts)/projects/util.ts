9c832dda8ee5fd45ea76b0cb2dcde884160001eb
elcharitas <jonathanirhodia@gmail.com>
2023-09-24 05:32:41 +0100

    feat: add extended support for project images

--- app/(posts)/projects/util.ts
+++ app/(posts)/projects/util.ts
@@ -14,18 +14,25 @@ export async function getAllProjects(): Promise<Post[]> {
       (repo: Repo) => !repo.fork && repo.description && repo.stargazers_count
     ) ?? [];
 
-  return filteredData
-    .sort((a, b) => b.stargazers_count - a.stargazers_count)
-    .map((repo) => ({
-      slug: repo.name,
-      title: `${repo.owner.login}/${repo.name}`,
-      brief: repo.description,
-      date: repo.pushed_at,
-      url: repo.html_url,
-      content: "",
-      coverImage: repo.owner.avatar_url,
-      type: "projects",
-      views: repo.stargazers_count,
-      owner: repo.owner.login,
-    }));
+  return Promise.all(
+    filteredData
+      .sort((a, b) => b.stargazers_count - a.stargazers_count)
+      .map(async (repo) => {
+        const coverImageUrl = `https://raw.githubusercontent.com/${repo.owner.login}/${repo.name}/${repo.default_branch}/static/logo-light.svg`;
+        const isCoverImage =
+          (await fetch(coverImageUrl, { method: "HEAD" })).status === 200;
+        return {
+          slug: repo.name,
+          title: `${repo.owner.login}/${repo.name}`,
+          brief: repo.description,
+          date: repo.pushed_at,
+          url: repo.html_url,
+          content: "",
+          coverImage: isCoverImage ? coverImageUrl : null,
+          type: "projects",
+          views: repo.stargazers_count,
+          owner: repo.owner.login,
+        };
+      })
+  );
 }

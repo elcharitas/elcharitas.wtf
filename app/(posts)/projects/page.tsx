4ea8bd92401d5673be391f77bec1e746d448c41a
elcharitas <jonathanirhodia@gmail.com>
2023-09-24 22:02:50 +0100

    chore: improve projects page tagline

--- app/(posts)/projects/page.tsx
+++ app/(posts)/projects/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
   return (
     <PostsPage
       title="Projects ⚡️"
-      description="These are some of the amazing projects I've worked or collaborated on. They're all on my github but require 1 star to show up here 😅."
+      description="These are some of the amazing projects I've worked or collaborated on. They're all on my github profile but require a stargazer or more to show up here 😅."
       featured={[featured, top1, top2]}
       sorted={projects}
     />

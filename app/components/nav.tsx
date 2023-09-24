e3aebc66ef46676bede92970e6391c672e9e6579
elcharitas <jonathanirhodia@gmail.com>
2023-09-24 12:30:31 +0100

    fix: mobile nav layout styles

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -28,10 +28,10 @@ export const Navigation: React.FC = () => {
   return (
     <header className="mb-8" ref={ref}>
       <div
-        className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${
+        className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b  ${
           isIntersecting
             ? "bg-zinc-900/0 border-transparent"
-            : "bg-zinc-900/500  border-zinc-800 "
+            : "bg-zinc-900/500 border-zinc-800"
         }`}
       >
         <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
@@ -40,7 +40,7 @@ export const Navigation: React.FC = () => {
               <Link
                 key={item.href}
                 href={item.href}
-                className="duration-500 text-xs md:text-sm text-zinc-400 hover:text-zinc-100 hover:scale-110"
+                className="hidden md:inline-block duration-500 text-xs md:text-sm text-zinc-400 hover:text-zinc-100 hover:scale-110"
               >
                 {item.name}
               </Link>
@@ -60,8 +60,8 @@ export const Navigation: React.FC = () => {
             href="/"
             className="text-zinc-300 hover:text-zinc-100 flex items-center gap-4 font-display text-xl text-transparent duration-1000 bg-white text-edge-outline animate-title whitespace-nowrap bg-clip-text "
           >
-            <ArrowLeft className="w-4 h-4 md:w-6 md:h-6 " />{" "}
-            <span className="hidden md:flex tracking-wider">elch🔥rit🔥s</span>
+            <ArrowLeft className="hidden md:flex md:w-6 md:h-6 " />{" "}
+            <span className="tracking-wider">elch🔥rit🔥s</span>
           </Link>
         </div>
       </div>

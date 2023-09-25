5bb0c610614030a4ec99d5895dfff0c27ee5ee5c
elcharitas <jonathanirhodia@gmail.com>
2023-09-25 17:19:17 +0100

    style: margin/padding fix on mobile

--- app/(posts)/layout.tsx
+++ app/(posts)/layout.tsx
@@ -9,7 +9,7 @@ export default function ProjectsLayout({
     <div className="relative min-h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black ">
       <div className="relative pb-16">
         <Navigation />
-        <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="md:px-6 pt-12 md:mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
           {children}
         </div>
       </div>

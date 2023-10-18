6b84bfb8639cbf3be7057ad18cbf811af207cbd7
elcharitas <jonathanirhodia@gmail.com>
2023-10-18 19:40:03 +0100

    feat: add projects count to header

--- app/(posts)/projects/page.tsx
+++ app/(posts)/projects/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   const [featured, top1, top2, ...projects] = await getAllProjects();
   return (
     <PostsPage
-      title="Projects ⚡️"
+      title={`${projects.length + 3} Projects ⚡️`}
       description="These are some of the amazing projects I've worked or collaborated on. They're all on my github profile but require a stargazer or more to show up here 😅."
       featured={[featured, top1, top2]}
       sorted={projects}

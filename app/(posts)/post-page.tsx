61b5d00c17da1e3ef008388fd47436a221de6b68
elcharitas <jonathanirhodia@gmail.com>
2023-10-19 07:08:33 +0100

    fix: improve project count interface

--- app/(posts)/post-page.tsx
+++ app/(posts)/post-page.tsx
@@ -8,6 +8,7 @@ import { Content } from "../components/content";
 
 interface PostsPageProps {
   title: string;
+  count?: number;
   description?: string;
   featured?: Post[];
   sorted: Post[];
@@ -18,6 +19,7 @@ interface PostsPageProps {
 
 export function PostsPage({
   title,
+  count,
   description,
   sorted,
   featured: [featured, ...others] = [],
@@ -28,7 +30,12 @@ export function PostsPage({
   return (
     <>
       <div className="max-w-2xl mx-auto lg:mx-0">
-        <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+        <h2 className="flex items-center text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+          {count && (
+            <span className="text-zinc-50 bg-zinc-600 p-2 mr-4 text-sm rounded-full">
+              {count}+
+            </span>
+          )}
           {title}
         </h2>
         <p className="mt-4 text-zinc-400">
--- app/(posts)/projects/page.tsx
+++ app/(posts)/projects/page.tsx
@@ -7,7 +7,8 @@ export default async function Page() {
   const [featured, top1, top2, ...projects] = await getAllProjects();
   return (
     <PostsPage
-      title={`${projects.length + 3} Projects ⚡️`}
+      title="Projects ⚡️"
+      count={projects.length + 3}
       description="These are some of the amazing projects I've worked or collaborated on. They're all on my github profile but require a stargazer or more to show up here 😅."
       featured={[featured, top1, top2]}
       sorted={projects}

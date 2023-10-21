2d6e6826c9c55be58402af4636bb36560cbff15d
elcharitas <jonathanirhodia@gmail.com>
2023-10-22 00:57:50 +0100

    feat: add driver.js to handle tours

---
+++ components/tour.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+import { driver, Side } from "driver.js";
+import { getUniqueSelector } from "./utils";
+import "driver.js/dist/driver.css";
+
+const driverObj = driver();
+
+export const TourDriver = () => {
+  const [isTourEnded, setIsTourEnded] = useState(false);
+
+  useEffect(() => {
+    const elements = Array.from(document.querySelectorAll("[data-tour]"));
+    const steps = elements.map((element) => ({
+      element: getUniqueSelector(element),
+      popover: {
+        title: element.getAttribute("data-tour-title") ?? "",
+        description: element.getAttribute("data-tour-description") ?? "",
+        side: element.getAttribute("data-tour-position") as Side,
+      },
+    }));
+    driverObj.setConfig({
+      steps,
+      popoverClass: "tour-wrapper",
+      onDestroyStarted: () => {
+        if (!driverObj.hasNextStep()) {
+          driverObj.destroy();
+          setIsTourEnded(true);
+        }
+      },
+    });
+    driverObj.drive();
+  }, []);
+
+  return isTourEnded ? null : <></>;
+};

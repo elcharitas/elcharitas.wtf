856e0de8c9ab2d6cc69bb2fd6d263072227d8566
elcharitas <jonathanirhodia@gmail.com>
2023-10-23 22:18:21 +0100

    feat: add webhook path to handle revalidation

---
+++ app/mods/webhook/route.ts
@@ -0,0 +1,31 @@
+import { getBlogPost } from "@/app/(parts)/blog/utils";
+import { revalidatePath } from "next/cache";
+
+interface WebhookData {
+  metadata: {
+    uuid: string;
+  };
+  data: {
+    eventType: "post_published" | "post_updated" | "post_deleted";
+    publication: {
+      id: string;
+    };
+    post: {
+      id: string;
+    };
+  };
+}
+
+export async function POST(request: Request) {
+  const body: WebhookData = await request.json();
+  const post = await getBlogPost(body.data.post.id);
+
+  if (
+    body.data.eventType === "post_published" ||
+    body.data.eventType === "post_deleted"
+  ) {
+    revalidatePath("/blog");
+  }
+
+  revalidatePath(`/blog/${post.slug}`);
+}

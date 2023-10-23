60d41bf09cf3ec463dae75e51053d3e892de57e2
elcharitas <jonathanirhodia@gmail.com>
2023-10-23 22:06:02 +0100

    fix: disable horizontal line after featured section

--- components/post-page.tsx
+++ components/post-page.tsx
@@ -64,72 +64,74 @@ export function PostsPage({
       <div className="w-full h-px bg-zinc-800" />
 
       {featured && (
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 items-center">
-          <Card>
-            <Link href={`/${featured.type}/${featured.slug}`}>
-              <article className="relative w-full h-full p-4 md:p-8">
-                <div className="flex items-center justify-between gap-2">
-                  <div className="text-xs text-zinc-100">
-                    {featured.date ? (
-                      <time dateTime={new Date(featured.date).toISOString()}>
-                        {Intl.DateTimeFormat("en-US", {
-                          dateStyle: "medium",
-                        }).format(new Date(featured.date))}
-                      </time>
-                    ) : (
-                      <span>SOON</span>
-                    )}
+        <>
+          <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 items-center">
+            <Card>
+              <Link href={`/${featured.type}/${featured.slug}`}>
+                <article className="relative w-full h-full p-4 md:p-8">
+                  <div className="flex items-center justify-between gap-2">
+                    <div className="text-xs text-zinc-100">
+                      {featured.date ? (
+                        <time dateTime={new Date(featured.date).toISOString()}>
+                          {Intl.DateTimeFormat("en-US", {
+                            dateStyle: "medium",
+                          }).format(new Date(featured.date))}
+                        </time>
+                      ) : (
+                        <span>SOON</span>
+                      )}
+                    </div>
+                    <span className="flex items-center gap-1 text-xs text-zinc-500">
+                      {featured.type === "projects" ? (
+                        <Star className="w-4 h-4" />
+                      ) : (
+                        <Eye className="w-4 h-4" />
+                      )}{" "}
+                      {Intl.NumberFormat("en-US", {
+                        notation: "compact",
+                      }).format(featured.views ?? 0)}
+                    </span>
                   </div>
-                  <span className="flex items-center gap-1 text-xs text-zinc-500">
-                    {featured.type === "projects" ? (
-                      <Star className="w-4 h-4" />
-                    ) : (
-                      <Eye className="w-4 h-4" />
-                    )}{" "}
-                    {Intl.NumberFormat("en-US", { notation: "compact" }).format(
-                      featured.views ?? 0
-                    )}
-                  </span>
-                </div>
 
-                {featured.coverImage && (
-                  <div className="relative rounded-lg overflow-hidden w-full h-48 mx-auto mt-2">
-                    <Image
-                      src={featured.coverImage}
-                      alt={featured.title}
-                      fill
-                    />
-                  </div>
-                )}
+                  {featured.coverImage && (
+                    <div className="relative rounded-lg overflow-hidden w-full h-48 mx-auto mt-2">
+                      <Image
+                        src={featured.coverImage}
+                        alt={featured.title}
+                        fill
+                      />
+                    </div>
+                  )}
 
-                <h2
-                  id="featured-post"
-                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
-                >
-                  {featured.title}
-                </h2>
-                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300 mb-8">
-                  {featured.brief}
-                </p>
-                <div className="absolute bottom-4 md:bottom-8">
-                  <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
-                    Read more <span aria-hidden="true">&rarr;</span>
+                  <h2
+                    id="featured-post"
+                    className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                  >
+                    {featured.title}
+                  </h2>
+                  <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300 mb-8">
+                    {featured.brief}
                   </p>
-                </div>
-              </article>
-            </Link>
-          </Card>
+                  <div className="absolute bottom-4 md:bottom-8">
+                    <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                      Read more <span aria-hidden="true">&rarr;</span>
+                    </p>
+                  </div>
+                </article>
+              </Link>
+            </Card>
 
-          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-            {others.map((post) => (
-              <Card key={post.slug}>
-                <Article post={post} showReadMore />
-              </Card>
-            ))}
+            <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+              {others.map((post) => (
+                <Card key={post.slug}>
+                  <Article post={post} showReadMore />
+                </Card>
+              ))}
+            </div>
           </div>
-        </div>
+          <div className="hidden w-full h-px md:block bg-zinc-800" />
+        </>
       )}
-      <div className="hidden w-full h-px md:block bg-zinc-800" />
 
       <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
         <div className="grid grid-cols-1 gap-4">

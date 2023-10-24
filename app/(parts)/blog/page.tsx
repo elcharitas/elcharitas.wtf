d102d560795ad2ab153eee9faf7969f30b142f65
elcharitas <jonathanirhodia@gmail.com>
2023-10-24 06:50:31 +0100

    fix: reduce blog cache time

--- app/(parts)/blog/page.tsx
+++ app/(parts)/blog/page.tsx
@@ -1,7 +1,7 @@
 import Blogs from "./blogs";
 import { getAllBlogs } from "./utils";
 
-export const revalidate = 60 * 60 * 24 * 7; // 1 week;
+export const revalidate = 60 * 60 * 24; // 1 day;
 
 export default async function Page() {
   const [blogPosts, initialCursor] = await getAllBlogs();

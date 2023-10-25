ecb822143397d3571f70c706cf4045f90b9e8192
elcharitas <jonathanirhodia@gmail.com>
2023-10-25 21:15:33 +0100

    fix: reduce cache validation to 1hour

--- app/(parts)/blog/page.tsx
+++ app/(parts)/blog/page.tsx
@@ -1,7 +1,7 @@
 import Blogs from "./blogs";
 import { getAllBlogs } from "./utils";
 
-export const revalidate = 60 * 60 * 24; // 1 day;
+export const revalidate = 60 * 60; // 1 hour;
 
 export default async function Page() {
   const [blogPosts, initialCursor] = await getAllBlogs();

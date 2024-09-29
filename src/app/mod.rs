73095393446ebca1f923b37124a5564835176cd9
elcharitas <jonathanirhodia@gmail.com>
2024-09-29 18:49:52 +0100

    update deps

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -4,9 +4,15 @@ mod newsleter;
 mod projects;
 
 use home::Home;
-use ngyn::{prelude::*, shared::core::redirect_to};
+use ngyn::prelude::*;
 
 pub fn register_routes(app: &mut HyperApplication) {
     app.get("/", handler(Home::route_handler));
-    app.get("/blog", redirect_to("/writings"))
+    app.get("/blog", redirect_permanent("/writings"));
+    app.get(
+        "/resume",
+        redirect_to(
+            "https://docs.google.com/document/d/1DwEEKQcO5RLs0bA55XTM-hOj4jYWQTTJlvZ7RD9tD-w/edit",
+        ),
+    );
 }

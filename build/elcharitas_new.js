fa90bab22feaf2a396d69b0ea43bcead148798ce
elcharitas <jonathanirhodia@gmail.com>
2026-06-24 15:13:39 +0100

    fix: update WebAssembly instance initialization and import structure in entry files

--- build/elcharitas_new.js
+++ build/elcharitas_new.js
@@ -1,7 +1,8 @@
 import wasm from "./elcharitas_new_bg.wasm";
-export * from "./elcharitas_new_bg.js";
+import * as __wbg from "./elcharitas_new_bg.js";
 import { __wbg_set_wasm } from "./elcharitas_new_bg.js";
+export * from "./elcharitas_new_bg.js";
 
-const instance = new WebAssembly.Instance(wasm, {});
+const instance = new WebAssembly.Instance(wasm, { "./elcharitas_new_bg.js": __wbg });
 __wbg_set_wasm(instance.exports);
 instance.exports.__wbindgen_start();
